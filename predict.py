93a295e785a15b5b8f111897472f2824cc3c5ba2
Archisahu27 <archisahu321@gmail.com>
2026-05-01 12:05:02 +0530

    fix model loading path

--- predict.py
+++ predict.py
@@ -1,20 +1,25 @@
 # predict.py
 
 import pickle
+import os
 
-# Load trained model
-model = pickle.load(open("model.pkl", "rb"))
+# Get absolute path of current file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Correct path to model
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+# Load model safely
+model = pickle.load(open(model_path, "rb"))
+
 
 def predict_price(rm, lstat, ptratio):
-    # Input must be 2D list
     features = [[rm, lstat, ptratio]]
-    
     prediction = model.predict(features)[0]
-    
     return round(prediction, 2)
 
 
-# Test (temporary)
+# Test locally
 if __name__ == "__main__":
     price = predict_price(6.5, 12.0, 15.0)
     print("Predicted Price:", price)
